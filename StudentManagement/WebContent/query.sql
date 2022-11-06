2385f4a5fde3d9bb31597aecdcaa2c91b0e62254
neneong <pukkuk12@naver.com>
2022-11-07 00:26:24 +0900

    /

--- StudentManagement/WebContent/query.sql
+++ StudentManagement/WebContent/query.sql
@@ -1,32 +1,32 @@
-create table member(
-	userid varchar2(20),
-	username varchar2(20),
-	usertype char(1),
-	
-	primary key(userid)
-
-
-);
-
 create table class(
 	classId varchar2(20),
 	className varchar2(20),
 	classInfo varchar2(100),
 	dates varchar2(20)
-)
+);
 
 insert into class values('1', 'alpha', 'aaaaaaaa', '2022-11-01');
 
---create table student(
---	userid varchar2(20),
---	classId varchar2(20)
---);
---
---
---create table teacher(
---	userid varchar2(20),
---	classId varchar2(20)
---);
+create table student(
+	studentid varchar2(20),
+	classId varchar2(20),
+	birth varchar2(20),
+	studentname varchar2(20)
+);
+
+
+create table teacher(
+	userid varchar2(20),
+	username varchar2(20),
+	usertype char(1),
+	
+	primary key(userid)
+);
+
+create table middle(
+	userid varchar2(20),
+	classid varhcar2(20)
+);
 
 insert into member values('admin', '관리자');
 insert into member values('user1', '금나라');
