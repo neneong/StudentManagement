8939d169382448bc58ba4d159715ed808c678490
Your Name <you@example.com>
2022-11-08 18:44:04 +0900

    .

--- StudentManagement/src/controller/UserLoginController.java
+++ StudentManagement/src/controller/UserLoginController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class UserLoginController
  */
-@WebServlet("/UserLoginController")
+@WebServlet("/userLogin")
 public class UserLoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
