a9451226f316f2df07edaa70148b0544b9c99c7b
neneong <pukkuk12@naver.com>
2022-11-15 23:03:45 +0900

    Update StudentTagDAO.java

--- StudentManagement/src/dao/StudentTagDAO.java
+++ StudentManagement/src/dao/StudentTagDAO.java
@@ -8,124 +8,95 @@ import java.util.ArrayList;
 
 import common.JdbcUtil;
 import vo.ClassVO;
+import vo.StudentTagVO;
+import vo.StudentVO;
 
-public class StudentTagDAO {
-	public void selectStudentTag(String classid){
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		ClassVO classes = new ClassVO();
-		
-		conn = JdbcUtil.getConnection();
-		
-		try {
-			pstmt = conn.prepareStatement("select classname, dates from class where classid=?");
-			pstmt.setString(1, classid);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				classes.setClassName(rs.getString("classname"));
-				classes.setDate(rs.getString("dates"));
-			}
-		}catch(SQLException e) {
-			e.printStackTrace();
-			
-		}finally {
-			JdbcUtil.close(conn, pstmt, rs);
-		}
-		
-		return;
-	}
-	
-
-	
-	public ArrayList<ClassVO> selectClassById(String classId){
-		ArrayList<ClassVO> classList = new ArrayList <ClassVO>();
+public class StudentTagDAO {	
+	public ArrayList<StudentTagVO> selectClassById(String classId){
+		ArrayList<StudentTagVO> studentList = new ArrayList <StudentTagVO>();
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		ClassVO classes = null;
+		StudentTagVO classes = null;
 		
 		conn = JdbcUtil.getConnection();
 		
 		try {
-			pstmt = conn.prepareStatement("select classid, classname, classinfo, dates from class where dates LIKE ? order by classid");
-			pstmt.setString(1, year + "%");
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				classes = new ClassVO();
-				classes.setClassId(rs.getString("classid"));
-				classes.setClassName(rs.getString("classname"));
-				classes.setDate(rs.getString("dates"));
-				classList.add(classes);
-			}
-		}catch(SQLException e) {
-			e.printStackTrace();
-			
-		}finally {
-			JdbcUtil.close(conn, pstmt, rs);
-		}
-		
-		return classList;
-	}
-	
-	public int insertClass(ClassVO vo) {
-		int n = 0;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "insert into class(classId, classPw, className, dates) values(?,?,?,?)";
-		
-		conn = JdbcUtil.getConnection();
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, vo.getClassId());
-			pstmt.setString(2, vo.getClassPw());
-			pstmt.setString(3, vo.getClassName());
-			pstmt.setString(4, vo.getDate());
-			n = pstmt.executeUpdate();
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JdbcUtil.close(conn, pstmt);
-		}
-		return n;
-	}
-	
-	public int updateClass(ClassVO vo) {
-		int n = 0;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "update class set className=? where classId=?";
-		
-		conn = JdbcUtil.getConnection();
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, vo.getClassId());
-			pstmt.setString(2, vo.getClassName());
-			n = pstmt.executeUpdate();
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JdbcUtil.close(conn, pstmt);
-		}
-		return n;
-	}
-	
-	public int deleteClass(String classId) {
-		int n = 0;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "delete from class where classId=?";
-		
-		conn = JdbcUtil.getConnection();
-		
-		try {
-			pstmt = conn.prepareStatement(sql);
+			pstmt = conn.prepareStatement("select userid from class where classid=?");
 			pstmt.setString(1, classId);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				classes = new StudentTagVO();
+				classes.setClassId(rs.getString("userid"));
+				studentList.add(classes);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}finally {
+			JdbcUtil.close(conn, pstmt, rs);
+		}
+		
+		return studentList;
+	}
+	
+	public int insertClassTag(StudentTagVO vo) {
+		int n = 0;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into class(userId, classId) values(?,?)";
+		
+		conn = JdbcUtil.getConnection();
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getStudentId());
+			pstmt.setString(2, vo.getClassId());
+			n = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JdbcUtil.close(conn, pstmt);
+		}
+		return n;
+	}
+	
+//	public int updateClass(StudentTagVO vo) {
+//		int n = 0;
+//		
+//		Connection conn = null;
+//		PreparedStatement pstmt = null;
+//		String sql = "update class set classId=? where userId=?";
+//		
+//		conn = JdbcUtil.getConnection();
+//		try {
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, vo.getStudentId());
+//			pstmt.setString(2, vo.getClassId());
+//			n = pstmt.executeUpdate();
+//		}catch(SQLException e) {
+//			e.printStackTrace();
+//		}finally {
+//			JdbcUtil.close(conn, pstmt);
+//		}
+//		return n;
+//	}
+	
+	public int deleteClass(String userId, String classId) {
+		int n = 0;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "delete from class where userId=? AND classId=?";
+		
+		conn = JdbcUtil.getConnection();
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, classId);
 			n = pstmt.executeUpdate();
 		}catch(SQLException e) {
 			e.printStackTrace();
